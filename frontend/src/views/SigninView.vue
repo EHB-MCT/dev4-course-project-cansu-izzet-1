3758ef73b16c255ddadcadc0a144b45dd36d7619
Cansu-1907 <56121553+Cansu-1907@users.noreply.github.com>
2023-05-08 22:42:22 +0200

    Sign in view completed and used the router-link to navigate

--- frontend/src/views/SigninView.vue
+++ frontend/src/views/SigninView.vue
@@ -1,23 +1,23 @@
 <template>
   <form id="loginForm" class="form">
-    <h1>LOGIN</h1>
+    <h1>SIGN IN</h1>
     <div class="formInputsContainer">
       <div class="formInputContainers">
         <label for="username">Username</label>
         <input type="text" id="username" placeholder="John" />
       </div>
       <div class="formInputContainers">
-        <label for="username">Password</label>
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="john@gmail.com" />
+      </div>
+      <div class="formInputContainers">
+        <label for="password">Password</label>
         <input type="password" id="password" placeholder="*******" />
       </div>
     </div>
     <div class="formButtonContainer">
       <button type="submit">SUBMIT</button>
-      <p>
-        Don't have an account yet?<router-link to="/signin"
-          >SIGN IN</router-link
-        >
-      </p>
+      <p>Already have an account?<router-link to="/">LOG IN</router-link></p>
     </div>
   </form>
 </template>
