dd0f0627c12fa4dfe30bd368f72b89d0c5c4c67d
Cansu-1907 <56121553+Cansu-1907@users.noreply.github.com>
2023-05-08 22:16:42 +0200

    Login view completed and added vue-router

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <form id="loginForm" class="form">
+    <h1>LOGIN</h1>
+    <div class="formInputsContainer">
+      <div class="formInputContainers">
+        <label for="username">Username</label>
+        <input type="text" id="username" placeholder="John" />
+      </div>
+      <div class="formInputContainers">
+        <label for="username">Password</label>
+        <input type="password" id="password" placeholder="*******" />
+      </div>
+    </div>
+    <div class="formButtonContainer">
+      <button type="submit">SUBMIT</button>
+      <p>Don't have an account yet?<a href="signin.html">SIGN IN</a></p>
+    </div>
+  </form>
+</template>
